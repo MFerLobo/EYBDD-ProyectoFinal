73c7b1188e5c72bb5d8cecb5fe2bedbf2befec5c
SM03594 <santiago.margarit@alu.inspt.utn.edu.ar>
2025-11-14 18:57:34 -0300

    Condicion de año actual añadido a consultas que obtienen informacion por mes

--- scipts SQL/consultas.sql
+++ scipts SQL/consultas.sql
@@ -9,7 +9,10 @@ SELECT
 FROM detalle_pedido
 INNER JOIN producto ON detalle_pedido.id_producto = producto.id_producto
 JOIN ticket ON detalle_pedido.id_pedido = ticket.id_ticket
-WHERE MONTH(ticket.fecha) = MONTH(NOW())
+WHERE 
+    YEAR(ticket.fecha) = YEAR(NOW())
+    AND 
+	MONTH(ticket.fecha) = MONTH(NOW())
 GROUP BY producto.id_producto
 ORDER BY SUM(detalle_pedido.cantidad) DESC;
 
@@ -19,14 +22,20 @@ SELECT
     ROUND(SUM(ticket.precio_final * impuesto.tasa), 2) AS 'Tributo IVA del mes'
 FROM ticket JOIN impuesto_ticket ON ticket.id_ticket = impuesto_ticket.id_ticket
 JOIN impuesto ON impuesto_ticket.id_impuesto = impuesto.id_impuesto
-WHERE MONTH(ticket.fecha) = MONTH(NOW());
+WHERE 
+    YEAR(ticket.fecha) = YEAR(NOW())
+    AND 
+	MONTH(ticket.fecha) = MONTH(NOW());
 
 -- Total de recaudación de las mesas por mes (actual o reemplazar MONTH(NOW()) por número de mes a consultar)
 SELECT 
 	SUM(ticket.precio_final) AS 'Recaudación de pedidos de mesas en el mes'
 FROM ticket
 INNER JOIN pedido_mesa ON ticket.id_pedido = pedido_mesa.id_pedido
-WHERE MONTH(ticket.fecha) = MONTH(NOW());
+WHERE 
+    YEAR(ticket.fecha) = YEAR(NOW())
+    AND 
+	MONTH(ticket.fecha) = MONTH(NOW());
     
     
 -- Total de recaudación de pedidos a domicilio por mes (actual o reemplazar MONTH(NOW()) por número de mes a consultar)
@@ -34,7 +43,10 @@ SELECT
 	SUM(ticket.precio_final) AS 'Recaudación de pedidos a domicilio en el mes'
 FROM ticket
 INNER JOIN pedido_domicilio ON ticket.id_pedido = pedido_domicilio.id_pedido
-WHERE MONTH(ticket.fecha) = MONTH(NOW());
+WHERE 
+    YEAR(ticket.fecha) = YEAR(NOW())
+    AND 
+	MONTH(ticket.fecha) = MONTH(NOW());
     
 -- Total recaudado en el año (actual o reemplazar YEAR(NOW()) por número de año a consultar), agrupado por recaudación mensual.
 SELECT
